86bbd1d3a4619d5a1c233cd4f68df5046775df83
lukass1337 <52844650+lukass1337@users.noreply.github.com>
2019-07-26 23:14:36 +0200

    Update mousecoords.py

--- mousecoords.py
+++ mousecoords.py
@@ -2,22 +2,22 @@ import mouse
 import keyboard
 import time
 
-running = True
-text_file = open("Output.txt", "a")
 delete = input("Do you want to empty the Output.txt file? (y/n) ")
+running = True
 
-if delete == "y":
+if delete.lower() == "y":
     text_file.truncate(0)
     print("\nOutput.txt has been emptied.\n")
 else:
     print("\nOutput.txt has not been emptied.\n")
 
-while running:
-    position = mouse.get_position()
-    if mouse.is_pressed(button="left"):
-        text_file.write(str(position) + ("\n"))
-        print("You have saved the coord: " + (str(position)))
-        time.sleep(1)
-    if keyboard.is_pressed("space"):
-        running = False
-        text_file.close()
+with open("Output.txt", "a") as file:
+    while running:
+        position = mouse.get_position()
+        if mouse.is_pressed(button="left"):
+            file.write(str(position) + ("\n"))
+            print("You have saved the coord: " + (str(position)))
+            time.sleep(1)
+        if keyboard.is_pressed("space"):
+            running = False
+            file.close()
